b357a447785d607e9996c3dea2aa322a84436482
haridy29 <mohamed.haridi.14@gmail.com>
2023-09-15 19:11:20 +0300

    * update security cors configuration issue to be integrated with the frontend. * Add ResponseMessage class, The class will be used to return response json message so it will be easy to handle in the frontend.

--- src/main/java/com/example/classbridge/config/security/SecurityConfig.java
+++ src/main/java/com/example/classbridge/config/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -26,8 +27,15 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).headers(header ->
+                        header.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
+                                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST, GET"))
+                                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Max-Age", "3600"))
+                                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
+                                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Expose-Headers", "Authorization"))
+                                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization"))
+                );
+        http.cors(cors -> cors.configure(http));
         return http.build();
     }
 
--- src/main/java/com/example/classbridge/controllers/HomeController.java
+++ src/main/java/com/example/classbridge/controllers/HomeController.java
@@ -2,27 +2,30 @@ package com.example.classbridge.controllers;
 
 import com.example.classbridge.dtos.LoginDto;
 import com.example.classbridge.services.AuthenticationService;
+import com.example.classbridge.utilities.ResponseMessage;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class HomeController {
     private final AuthenticationService service;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(HttpServletResponse response, @Valid @RequestBody LoginDto loginDTO) {
+    public ResponseEntity<ResponseMessage> login(HttpServletResponse response, @Valid @RequestBody LoginDto loginDTO) {
         try {
             service.login(response, loginDTO);
-            return ResponseEntity.ok("Success");
+            return ResponseEntity.ok(new ResponseMessage("Success"));
         } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid username or password");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ResponseMessage("Invalid username or password"));
         }
     }
 
